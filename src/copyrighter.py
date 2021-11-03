ef7a98ea689ed0ef403f798a0c1858df06698f42
Nico_ <n.lepetit1@gmail.com>
2021-11-03 19:17:42 +0100

    copyrighter v1

--- src/copyrighter.py
+++ src/copyrighter.py
@@ -1,6 +1,27 @@
+from os.path import splitext
+from glob import glob
+from tqdm import tqdm
+
+def prepend(filename, new_content):
+    f = open(filename, mode='r+')
+    content = f.read()
+    f.seek(0)
+    f.write(new_content + content)
+
 class coPyRighter:
     def __init__(self, folder : str = ''):
         self._folder = folder
+        self._files = glob(folder+"/**/*.*", recursive = True)
+        self._cpp = open("/Users/NicolasLepetit/coPyRight/copyrights/cpp.txt", mode='r').read()
+        self._py = open("/Users/NicolasLepetit/coPyRight/copyrights/py.txt", mode='r').read()
 
     def do(self):
-        print("I'm doing")
+        for f in tqdm(self._files):
+            ext = splitext(f)[1]
+            if ext == ".py":
+                prepend(f, self._py)
+            elif ext == ".cpp":
+                prepend(f, self._cpp)
+            else:
+                print("currently unsupported file format. Please add it to copyrights")
+
